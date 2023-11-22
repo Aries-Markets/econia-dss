05fa8e1d632554e6062c7db48958c17b80b6da90
Aliver <aliver.len@outlook.com>
2023-11-22 17:27:35 +0800

    chore: key

--- src/kubernetes/cmd.sh
+++ src/kubernetes/cmd.sh
@@ -2,6 +2,23 @@
 
 dir=$(dirname $(realpath $0))
 
+export TESTNET_API_KEY="your key"
+export MAINNET_API_KEY="your key"
+
+function api_key() {
+    case "$1" in
+        "testnet")
+            echo "$TESTNET_API_KEY"
+            ;;
+        "mainnet")
+            echo "$MAINNET_API_KEY"
+            ;;
+        *)
+            echo "Invalid parameter. Please specify testnet or mainnet."
+            ;;
+    esac
+}
+
 function build() {
     kustomize build \
     --load-restrictor LoadRestrictionsNone \
@@ -11,11 +28,18 @@ function build() {
 
 function apply() {
     build $1
+
+    key=$(api_key $1)
+
+    sed -i 's@$API_KEY@'$key'@g' $dir/manifest/$1/artifacts/*
+
     if [[ -z "$2" ]]; then
         kubectl apply -f "$dir/manifest/$1/artifacts/"
     else
         kubectl apply -f "$dir/manifest/$1/artifacts/*$2.yaml"
     fi
+
+    sed -i 's@'$key'@$API_KEY@g' $dir/manifest/$1/artifacts/*
 }
 
 case "$1" in
