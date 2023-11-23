5cecaff22275f16a517cf7a539e1cf9c77772721
Alivers <aliver.len@outlook.com>
2023-11-23 11:52:13 +0800

    chore: images

--- src/kubernetes/images.sh
+++ src/kubernetes/images.sh
@@ -6,7 +6,7 @@ dir=$(dirname $(realpath $0))
 
 IMAGE_TAG="latest"
 
-export DOCKER_REGISTRY="854153369854.dkr.ecr.ap-southeast-1.amazonaws.com/econia-dss"
+export DOCKER_REGISTRY="ariesethan/econia-dss"
 export DOCKER_DIR=$dir/../../src/docker
 export PROJECT_DIR=$dir/../..
 
@@ -18,21 +18,21 @@ function ecr-auth() {
 function aggregator() {
     docker build \
     -f $DOCKER_DIR/aggregator/Dockerfile \
-    -t $DOCKER_REGISTRY/aggregator:$IMAGE_TAG \
+    -t $DOCKER_REGISTRY-aggregator:$IMAGE_TAG \
     $PROJECT_DIR
 }
 
 function postgres() {
     docker build \
     -f $DOCKER_DIR/database/Dockerfile.postgres \
-    -t $DOCKER_REGISTRY/postgres:$IMAGE_TAG \
+    -t $DOCKER_REGISTRY-postgres:$IMAGE_TAG \
     $PROJECT_DIR
 }
 
 function diesel() {
     docker build \
     -f $DOCKER_DIR/database/Dockerfile.diesel \
-    -t $DOCKER_REGISTRY/diesel:$IMAGE_TAG \
+    -t $DOCKER_REGISTRY-diesel:$IMAGE_TAG \
     --build-arg "DATABASE_URL=postgres://econia:econia@postgres:5432/econia" \
     $PROJECT_DIR
 }
@@ -40,14 +40,14 @@ function diesel() {
 function processor() {
     docker build \
     -f $DOCKER_DIR/processor/Dockerfile \
-    -t $DOCKER_REGISTRY/processor:$IMAGE_TAG \
+    -t $DOCKER_REGISTRY-processor:$IMAGE_TAG \
     $PROJECT_DIR
 }
 
 function ws() {
     docker build \
     -f $DOCKER_DIR/api/Dockerfile.ws \
-    -t $DOCKER_REGISTRY/ws:$IMAGE_TAG \
+    -t $DOCKER_REGISTRY-ws:$IMAGE_TAG \
     --build-arg POSTGRES_WEBSOCKETS_VERSION=0.11.1.0 \
     $PROJECT_DIR
 }
@@ -55,12 +55,12 @@ function ws() {
 function postgrest() {
     docker build \
     -f $DOCKER_DIR/api/Dockerfile.postgrest \
-    -t $DOCKER_REGISTRY/postgrest:$IMAGE_TAG \
+    -t $DOCKER_REGISTRY-postgrest:$IMAGE_TAG \
     $PROJECT_DIR
 }
 
 function push() {
-    docker push $DOCKER_REGISTRY/$1:$IMAGE_TAG 
+    docker push $DOCKER_REGISTRY-$1:$IMAGE_TAG 
 }
 
 case "$1" in
